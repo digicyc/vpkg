87dd88cdf9ec7c230ed1a950eaeaae2fd775127a
AaronA <digicyc@gmail.com>
2025-04-06 22:58:12 -0600

    Add our Pkg Search function.

--- pkg.go
+++ pkg.go
@@ -1,9 +1,38 @@
 package main
 
+import (
+	"log"
+	"os/exec"
+	"regexp"
+	"strings"
+)
 
-func searchPkg(name string) string {
+func searchPkg(pkgName string) map[string]string {
     // Pass in package name to search for
     // Pull proper name and description.
+    re := regexp.MustCompile("[ ]{2,}\\w")
+    cmdString := "xbps-query -Rs " + pkgName
+    cmd := exec.Command(cmdString)
+    pkgmap := map[string]string{}
+    stdout, err := cmd.Output()
+    if err != nil {
+        log.Fatal(err)
+    }
 
-   return "This is a dummy package for " + name
+    res := string(stdout)
+    res_list := strings.Split(res, "[-]")
+    for i := range res_list {
+        pkginfo := re.Split(res_list[i], -1)
+        if len(pkginfo) > 1 {
+            pkgmap[pkginfo[0]] = pkginfo[1]
+        }
+    }
+
+   return pkgmap
+}
+
+func installPkg(pkgName string) string {
+    // Update the repos
+    // xbps-install -Su
+    return "a pkg yay!"
 }
